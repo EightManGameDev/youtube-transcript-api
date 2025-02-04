ad3d79c5a5d08e790fdbd598a13befa7cf5e1224
EightManGameDev <99240068+EightManGameDev@users.noreply.github.com>
2025-02-04 10:53:05 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,41 +1,30 @@
-import axios from "axios";
-import cheerio from "cheerio";
+import express from "express";
+import TranscriptAPI from "./index.js";
 
-class TranscriptAPI {
-  static async getTranscript(id, config = {}) {
-    const url = new URL('https://youtubetranscript.com');
-    url.searchParams.set('server_vid2', id);
-    
-    const
-      response = await axios.get(url, config),
-      $ = cheerio.load(response.data, undefined, false),
-      err = $('error');
-  
-    if (err.length) throw new Error(err.text());
-    return $('transcript text').map((i, elem) => {
-      const $a = $(elem);
-      return {
-        text: $a.text(),
-        start: Number($a.attr('start')),
-        duration: Number($a.attr('dur'))
-      };
-    }).toArray();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+console.log(`Server starting on port: ${PORT}`); // Debugging log
+
+app.get('/get_transcript', async (req, res) => {
+  const videoId = req.query.video_id;
+
+  if (!videoId) {
+    return res.status(400).send({ error: "Missing video_id parameter" });
   }
 
-  static async validateID(id, config = {}) {
-    const url = new URL('https://video.google.com/timedtext');
-    url.searchParams.set('type', 'track');
-    url.searchParams.set('v', id);
-    url.searchParams.set('id', 0);
-    url.searchParams.set('lang', 'en');
-    
-    try {
-      await axios.get(url, config);
-      return !0;
-    } catch (_) {
-      return !1;
-    }
+  try {
+    const transcript = await TranscriptAPI.getTranscript(videoId);
+    res.json(transcript);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
   }
-}
+});
 
-export { TranscriptAPI as default };
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
